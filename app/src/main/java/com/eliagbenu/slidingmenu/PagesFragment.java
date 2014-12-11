7c50e129a29465a214b56e72d79d3fc92291b3ba
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2014-12-11 10:51:51 +0000

    did sliding menu demo

---
+++ app/src/main/java/com/eliagbenu/slidingmenu/PagesFragment.java
@@ -0,0 +1,23 @@
+package com.eliagbenu.slidingmenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by eli on 11/14/14.
+ */
+public class PagesFragment  extends Fragment{
+    public PagesFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
